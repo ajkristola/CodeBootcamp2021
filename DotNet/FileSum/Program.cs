52596cf1a2a3b9c0adb8a19d5c44d77c2601ae4d
ajkristola <aleksi.kristola@gmail.com>
2021-10-27 19:07:16 +0300

    Added an alternative TryParse implementation and slightly modified numbers.txt.

--- DotNet/FileSum/Program.cs
+++ DotNet/FileSum/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Runtime;
 
 namespace FileSum
 {
@@ -12,8 +11,9 @@ namespace FileSum
 
             string filename = @"numbers.txt";
             string[] lines = File.ReadAllLines(filename);
+
             int sum = 0;
-            int currentLine =1;
+            int currentLine = 1;
 
             // Loops through each line and parses them into new int 'value' that changes.
             // Then adds the value to 'sum' and loops again to retrieve the new line value. Get me?
@@ -22,20 +22,41 @@ namespace FileSum
             {
                 try
                 {
-                    int value = int.Parse(line); 
+                    int value = int.Parse(line);
                     sum += value;
                 }
                 catch
                 {
-            
-            // Try to understand this better:
+                    Console.WriteLine("Could not read line \"" + line + "\" on line "
+                                        + currentLine + ", not a number.");
+                }
+                currentLine++;
+            }
 
-                    Console.WriteLine("Dammit. Cannot process line " + currentLine +
+
+
+            /* TryParse method
+            ------------------
+            using System.Runtime;
+            
+            foreach (string line in lines)
+            {
+                int value;
+
+                bool success = int.TryParse(line, out value);
+                if (success)
+                {
+                    sum += value;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot process line " + currentLine +
                                         " with value \"" + line + "\", not a valid number.");
                 }
 
                 currentLine++;
             }
+            */
 
             Console.WriteLine("Calculated sum: " + sum);
 
