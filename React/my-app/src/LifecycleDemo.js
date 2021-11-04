e57847ee3fb89057d9e805d1e927afa3ab78cdb2
ajkristola <aleksi.kristola@gmail.com>
2021-11-04 15:36:58 +0200

    Added json fetch call and state management.

--- React/my-app/src/LifecycleDemo.js
+++ React/my-app/src/LifecycleDemo.js
@@ -5,15 +5,27 @@ class LifecycleDemo extends React.Component {   // class declaration
     constructor(props) {
         super(props);
         console.log("In LifecycleDemo.constructor. Called always first, irrespective or order.");
+        this.state = { title: "" };
     }
-       
-    render() {                                  // method for showing HTML stuff
-        console.log("In LifecycleDemo.render. Called second");
-        return <h1>Hello, this is the Lifecycle component!</h1>
+    
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json.title)
+            this.setState({ title: json.title });
+            console.log("Component state has been modified");
+        });
+        console.log("Fetch call has been made.");
     }
 
-    componentDidMount() {
-        console.log("In LifecycleDemo.componentDidMount. Called third.");
+    render() {                                  // method for showing HTML stuff
+        console.log("In LifecycleDemo.render. Called second");
+        return (
+        <div>
+            <h1>Hello, this is the Lifecycle component!</h1>
+            <p>Title: {this.state.title}</p>
+        </div>);
     }
 }
 
